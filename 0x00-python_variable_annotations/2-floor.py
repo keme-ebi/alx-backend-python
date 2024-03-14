04b8b901e370cd705e0d83a56c0cd5b08706a9c9
Remember <kelvin6bolou@gmail.com>
2024-03-14 22:50:48 +0100

    a type-annotated function that returns the floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+2-floor module
+"""
+
+def floor(n: float) -> int:
+    """
+    rounds a number to the nearest integer
+    Arg:
+        n(float): number to round up
+    Return:
+        the rounded number as an int
+    """
+    return (int(n // 1))
