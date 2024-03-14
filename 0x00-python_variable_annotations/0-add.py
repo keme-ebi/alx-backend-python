8ba6410858fccfd4d11cdaad3d1a56790de21922
Remember <kelvin6bolou@gmail.com>
2024-03-14 22:14:40 +0100

    added documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,12 @@
 """
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats together
+    Args:
+        a(float): first parameter
+        b(float): second parameter
+    Return:
+        addition of both floats as float
+    """
     return a + b
