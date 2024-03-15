a6ba0f5750dee67d1e3b264b92f0e3f68df8e133
keme-ebi <kelvin6bolou@gmail.com>
2024-03-15 03:48:34 +0100

    used mypy to validate code

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,11 +6,19 @@ from typing import Any, List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-        zoomed_in: List = [
-            item for item in lst
-            for i in range(factor)
-        ]
-        return zoomed_in
+    """
+    doubles the numbers from a tuple
+    Arg:
+        lst: tuple to go through
+        factor: number of times each number of element should be listed
+    Return:
+        list of zoomed array
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
 
 
 array = (12, 72, 91)
