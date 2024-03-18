5e6fbc3053f068adb70ea1479b61263c11836699
keme-ebi <kelvin6bolou@gmail.com>
2024-03-18 19:01:44 +0100

    a function that returns a asyncio.Task object

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+3-tasks
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    a function that returns a asyncio.Task
+    Arg:
+        max_delay(int): maximum delay for wait_random
+    Return:
+        asyncio.Task object
+    """
+    return (asyncio.create_task(wait_random(max_delay)))
