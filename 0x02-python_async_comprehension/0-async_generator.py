19dbef00b146f7f82642911c37cfecf3a89412a6
keme-ebi <kelvin6bolou@gmail.com>
2024-03-19 13:21:25 +0100

    change type annotation return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,15 +4,14 @@
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     loops 10 times, wait 1 second, then yield a random number between
         0 and 10
     """
     for i in range(10):
         await asyncio.sleep(1)
-        num = random.uniform(0, 10)
-        yield num
+        yield random.uniform(0, 10)
