87c30ef5781c46b3f756cc5c6591db49ea2c6490
keme-ebi <kelvin6bolou@gmail.com>
2024-04-08 23:11:47 +0100

    added test for KeyError with the access_nested_map function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """tests the access_nested_map function from utils"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
