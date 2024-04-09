8d02b68e58833cf8b38694f62aba6d4d72aceba0
keme-ebi <kelvin6bolou@gmail.com>
2024-04-09 22:03:53 +0100

    created a test class for GithubOrgClient and a test method to test GithubOrgClient.org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""tests the client module"""
+import unittest
+from client import GithubOrgClient as Goc
+from parameterized import parameterized
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """a class that inherits from unittest.TestCase in order to test
+        the GithubOrgClient class
+    """
+    @parameterized.expand([
+        ("google", {"Success": True}),
+        ("abc", {"Success": False})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, expected, mocked):
+        """tests the GithubOrgClien.org method"""
+        mocked.return_value = expected
+        test = Goc(org)
+        url = "https://api.github.com/orgs/{}".format(org)
+        self.assertEqual(test.org, mocked.return_value)
+        mocked.assert_called_once_with(url)
